0e93e58a0fd5996a14d7dbfb82bdbb7df72de5cb
EPoudevigne810 <epoudevigne@guardiaschool.fr>
2025-04-02 14:32:03 +0200

    models.py

--- models.py
+++ models.py
@@ -8,12 +8,11 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True)
     uid = Column(String(255), unique=True, nullable=False)
-    prenom = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False)
     role = Column(String(255), nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
 
 class Role(Base):
     __tablename__ = "roles"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    uid = Column(String(255), unique=True, nullable=False)
-    prenom = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False)
