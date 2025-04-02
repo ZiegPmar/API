a74185f8cbb69a5c07c6b8df4a8c26d9f868896a
EPoudevigne810 <epoudevigne@guardiaschool.fr>
2025-04-02 18:49:20 +0200

    models.py

--- models.py
+++ models.py
@@ -1,16 +1,17 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-import datetime
+from datetime import datetime
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    uid = Column(String(50), unique=True, nullable=False)      
-    name = Column(String(100), nullable=False)                 
-    role = Column(Integer, nullable=False)                     
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    uid = Column(String(50), unique=True, nullable=False)
+    name = Column(String(100), nullable=False)
+    role = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
 
 class Log(Base):
     __tablename__ = "logs"
